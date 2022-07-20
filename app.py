4f97e22845b9d31defa12be9d0797293783b5b98
Nicholas <nicholas.cruickshank25@gmail.com>
2022-07-20 21:28:21 +1000

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, render_template
+import gunicorn
+import bcrypt
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+    
