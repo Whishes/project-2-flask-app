3ea3e10be026505a022d791dee4ba38754294612
Nicholas <nicholas.cruickshank25@gmail.com>
2022-07-23 19:16:58 +1000

    page design mockups currently being worked on. Make stucture basically done, just cleaning out the finer details for when the backend is started

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # project-2-flask-app
+
+https://immense-chamber-64350.herokuapp.com/
--- app.py
+++ app.py
@@ -3,12 +3,29 @@ from flask import Flask, render_template
 import gunicorn
 import bcrypt
 import psycopg2
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL", 'dbname=niceities')
+SECRET_KEY = os.environ.get("SECRET_KEY", "niceities-backup")
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY.encode()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")   
+
+@app.route("/register")
+def regiser():
+    return render_template("register.html") 
+
+@app.route("/create")
+def create():
+    return render_template("create.html")   
     
 if __name__ == "__main__":
     app.run(debug=True)
